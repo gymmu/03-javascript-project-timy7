9375b2543a31e5846ef00e907470c463add31767
Yeva Skotar <e270611@edu.sbl.ch>
2023-10-31 12:25:36 +0200

    Ende der 1. Lektion

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
-export function aufgabe01(args) {
+export  function aufgabe01 (args){
   const input = args
   const result = []
 
-  for (let i = 0; i < input.length; i++) {
-    const currentElement = input[i]
+for (let i = 0; i < input.length; i++)
+ {const currentElement = input[i]
+   if (currentElement === "e")
+    {
+    result.push("")
+   } 
+   else if (currentElement === "E") {}
+   else {
     result.push(currentElement)
-  }
-  return result.join("")
+   }
+}
+return result.join("")
+}
+
+ export function aufgabe02 (args){
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++)
+   {const currentElement = input[i]
+  result.push(currentElement.toUpperCase())
+   }
+return result.join("")
+}
+
+export function aufgabe03 (args){
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++)
+   
+  {const currentElement = input[i]
+
+   }
+
+
+   return result.join("")
 }

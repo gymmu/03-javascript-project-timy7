a50d66db67544fe43ce278bf078d69c5c561c675
Yeva Skotar <e270611@edu.sbl.ch>
2023-11-28 11:41:46 +0200

    28.11 Erste Lektion

--- script.js
+++ script.js
@@ -26,15 +26,226 @@ return result.join("")
 return result.join("")
 }
 
-export function aufgabe03 (args){
+/* Alle e zählen*/
+
+export function aufgabe03(args) {
+  const input = args
+  let count = 0
+  for (let i = 0; i < input.length; i++)  {
+    const currentElement = input[i]
+    if (currentElement === "e") {
+      count = count + 1
+    } else if (currentElement === "E") {
+      count = count + 1
+    }
+  }
+  return count
+}
+
+
+/*Zähle alle Wörter in einem Text*/
+
+export function aufgabe04 (args){
+  const input = args
+  let count = 1
+  for (let i = 0; i < input.length; i++)  {
+    const currentElement = input[i]
+   
+    if (currentElement === " ") {
+      count = count + 1
+    }
+  }
+  return count
+}
+
+
+/*Schreibe eine Funktion, die testet, ob in einem Text mindestens ein
+Grossbuchstabe vorkommt. Fragen: wie ich eine Grossbuchstabe definiere?*/
+
+export function aufgabe05 (args){
+  const input = args
+  for (let i = 0; i < input.length; i++)  {
+    const currentElement = input[i]
+    if (currentElement === " ") {
+
+    } else if (currentElement === ".") {
+
+    } else if (currentElement.toUpperCase() === currentElement) {
+      return true
+    }
+  }
+  return false
+}
+
+
+
+export function aufgabe06 (args){
+  const input = args
+  for (let i = 0; i < input.length; i++)  {
+    const currentElement = input[i]
+    if (currentElement === ".") {
+      return true
+    }
+  }
+  return false
+}
+
+/*Schreibe eine Funktion, die testet, 
+ob in einem Text das Wort 'und' vorkommt.*/
+
+export function aufgabe07 (args){
+  const input = args
+  for (let i = 0; i < input.length; i++)  {
+    const currentElement = input[i]
+    
+    if (currentElement === "u") {
+      if (input[i+1] === "n")
+      {
+    if (input[i+2] === "d")
+    return true
+  }
+}
+
+  }
+  return false
+}
+
+export function aufgabe08 (args){
+  const input = args
+  const result =  []
+  for (let i = 0; i < input.length; i++)  {
+    const currentElement = input[i]
+    
+    if (currentElement === "e") {
+      result.push("3")
+    } else {
+     result.push(currentElement)
+  }
+}
+  return result.join("")
+}
+
+
+/*Teste ob eine Eingabe genau sechs Zeichen lang ist.*/
+export function aufgabe09 (args){
+  const input = args
+  
+  if (input.length === 6) {
+    return true
+  } 
+
+  return false
+}
+
+
+/*aufgabe 10
+Teste ob eine Eingabe ein korrekter RGB Hexcode ist.*/
+
+export function aufgabe10 (args){
+  const input = args
+
+  if (input.length !== 7) return false
+   if (input[0] !== "#") return false
+
+   for (let i = 1; i < input.length; i++) {
+    const currentElement = input[i]
+    const ascii = currentElement.charCodeAt(0)
+
+    if (48 <= ascii && ascii <= 57){
+      //ist eine Ziffer
+    } else if (65 <= ascii && ascii <= 70){
+      //ist a-f
+    } else {
+      return false 
+     }
+   }
+  return true 
+}
+
+/*Gib den ASCII code eines einzelnen Buchstabens an.*/
+export function aufgabe11 (args){
   const input = args
   const result = []
-  for (let i = 0; i < input.length; i++)
+
+  if (input.length > 1) return null
+  for (let i = 0; i < input.length; i++)  {
+    const currentElement = input[i]
+    const ascii = currentElement.charCodeAt(0)
+    return ascii
+}
+return null
+}
+
+/*Suche die Position des ersten `e`s in einem Text.*/ 
+
+export function aufgabe12 (args){
+  const input = args
+  let count = 0
+
+  for (let i = 0; i < input.length; i++)  {
+    const currentElement = input[i]
+
+    if (input[i] === "e"){
+      return i
+    }
+  }
+return -1
+}
+
+/*Suche die Position des letzten `e`s in einem Text.*/
+
+export function aufgabe13 (args){
+  const input = args
+  let count = 0
+
+  for (let i = input.length -1 ; i >=0 ; i--)  {
+    const currentElement = input[i]
+
+    if (input[i]  === "e"){
+      return i 
+    }
+  }
+return -1
+}
+
+/*Suche die Position des dritten `e`s in einem Text.*/
+
+export function aufgabe14 (args){
+  const input = args
+  let count = 0
+  const currentElement = input[i]
    
-  {const currentElement = input[i]
+  for (let i = 0; i < input.length; i++)  {
+   
+    if (currentElement === "e"){
+      count++
+      if (count === 3){
+        return i
+      }
+      if (count === 2){
+      return -1 }
+      
+      if (count === 1){
+        return -1 }
+    }
+    if (input.value = "e"){
+      return 2
+    }
+  }
+}
+/*Lesen Sie die Eingabe nur bis zum ersten Leerzeichen ein.
+*/
 
-   }
+export function aufgabe15 (args){
+  const input = args
 
+  for (let i = 0; i < input.length; i++)  {
+    const currentElement = input[i]
 
-   return result.join("")
+  if (currentElement === " "){
+  return currentElement
+  }
+
+  }
+return -1
 }
